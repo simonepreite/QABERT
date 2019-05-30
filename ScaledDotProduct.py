ff504c7cc4fe80191a42a56bf9a7921d7487e553
Simone <simone-preite_89@hotmail.it>
2019-05-30 17:02:27 +0200

    substitute spaces to tabs

--- ScaledDotProduct.py
+++ ScaledDotProduct.py
@@ -3,13 +3,13 @@ from torch import nn
 import math
 
 class ScaledDotProduct(nn.Module):
-    def __init__(self, attentionHeadSize, dropOutProb=0.1):
-        super(ScaledDotProduct, self).__init__()
-        self.attentionHeadSize = attentionHeadSize
-        self.dropout = nn.Dropout(dropOutProb)
-        
-    def forward(self, Q, K, V, attentionMask):
-        aScores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.attentionHeadSize)
-        aScores = aScores + attentionMask
-        aProbs = self.dropout(nn.Softmax(dim=-1)(aScores))
-        return torch.matmul(aProbs, V)
+	def __init__(self, attentionHeadSize, dropOutProb=0.1):
+		super(ScaledDotProduct, self).__init__()
+		self.attentionHeadSize = attentionHeadSize
+		self.dropout = nn.Dropout(dropOutProb)
+		
+	def forward(self, Q, K, V, attentionMask):
+		aScores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(self.attentionHeadSize)
+		aScores = aScores + attentionMask
+		aProbs = self.dropout(nn.Softmax(dim=-1)(aScores))
+		return torch.matmul(aProbs, V)

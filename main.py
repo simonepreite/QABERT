2b8f5ed8c2ecba15e76d59ff1d25de15ccf81c1a
Matteo <delvecchiomatteo96@gmail.com>
2019-06-11 23:22:45 +0200

    Focus on isImpossible part of the model

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from collections import namedtuple
 from tqdm import tqdm, trange
 import argparse
 from SQuADDataset import InputFeatures
+from utils import saveVocab
 
 
 def main():
@@ -33,7 +34,7 @@ def main():
 	trainFile = args.trainFile
 	predictFile = args.predictFile
 	modelWeights = args.modelWeights
-	trainBatchSize = 12
+	trainBatchSize = 16
 	numTrainEpochs = 1 # these both are parameter that have to come from the commmand line
 
 	#if torch.cuda.is_available()
@@ -97,6 +98,16 @@ def main():
 	evalExamples = readSQuADDataset(predictFile, False, squadV2=True)
 	evalFeatures = featurizeExamples(evalExamples, tokenizer, 512, 128, 256, False)
 
+	cachedEvalFeaturesFile = outputDir + "/evalFeatures.bin"
+	evalFeatures = None
+	try:
+		with open(cachedEvalFeaturesFile, "rb") as reader:
+			evalFeatures = pickle.load(reader)
+	except:
+		evalFeatures = featurizeExamples(evalExamples, tokenizer, 512, 128, 256, True) #to generalize paramenters
+		with open(cachedEvalFeaturesFile, "wb") as writer:
+			pickle.dump(evalFeatures, writer)
+
 	allInputIDs = torch.tensor([f.inputIDs for f in evalFeatures], dtype=torch.long)
 	allInputMask = torch.tensor([f.inputMask for f in evalFeatures], dtype=torch.long)
 	allSegmentIDs = torch.tensor([f.segmentIDs for f in evalFeatures], dtype=torch.long)
@@ -105,12 +116,13 @@ def main():
 	evalData = TensorDataset(allInputIDs, allInputMask, allSegmentIDs, allExampleIndex, allIsImpossible)
 
 	evalSampler = SequentialSampler(evalData)
-	evalDataLoader = DataLoader(evalData, sampler=evalSampler, batch_size=trainBatchSize)
+	evalDataLoader = DataLoader(evalData, sampler=evalSampler, batch_size=128)
 
 	evalBatchInputIDs, evalBatchInputMask, evalBatchSegmentIDs, evalBatchExampleIndices, evalIsImpossibles = next(iter(evalDataLoader))
 	evalBatchInputIDs = evalBatchInputIDs.to(device)
 	evalBatchInputMask = evalBatchInputMask.to(device)
 	evalBatchSegmentIDs = evalBatchSegmentIDs.to(device)
+	evalBatchExampleIndices = evalBatchExampleIndices.to(device)
 	evalIsImpossibles = evalIsImpossibles.to(device)
 
 
@@ -129,7 +141,7 @@ def main():
 	
 	print("Start training for isImpossible part")
 	# Training for the isImpossible part of the network
-	for epoch in range(int(numTrainEpochs)):
+	for epoch in trange(int(numTrainEpochs)):
 		for step, batch in enumerate(tqdm(trainDataLoader, desc="Iteration for IsImpossible")):
 			if nGPU >= 1:
 				batch = tuple(t.to(device) for t in batch)
@@ -160,26 +172,71 @@ def main():
 			optimizer.zero_grad()
 			globalStep += 1
 
-			precision = precision_score(isImpossibles.detach().cpu(), isImpossibleComputed.detach().cpu() > 0.5, average="micro")
-			recall = recall_score(isImpossibles.detach().cpu(), isImpossibleComputed.detach().cpu() > 0.5, average="micro")
-			f1 = f1_score(isImpossibles.detach().cpu(), isImpossibleComputed.detach().cpu() > 0.5, average="micro")
+			# precision = precision_score(isImpossibles.detach().cpu(), isImpossibleComputed.detach().cpu() > 0.5, average="micro")
+			# recall = recall_score(isImpossibles.detach().cpu(), isImpossibleComputed.detach().cpu() > 0.5, average="micro")
+			# f1 = f1_score(isImpossibles.detach().cpu(), isImpossibleComputed.detach().cpu() > 0.5, average="micro")
 
-			tqdm.write("Step: {} - Loss: {}, Precision: {}, Recall: {}, F1: {}".format(step, loss, precision, recall, f1))
+			# tqdm.write("Step: {} - Loss: {}, Precision: {}, Recall: {}, F1: {}".format(step, loss, precision, recall, f1))
+
+		# with torch.no_grad():
+		# 	batchStartLogits, batchEndLogits, batchIsImpossible = model(evalBatchInputIDs, evalBatchInputMask, evalBatchSegmentIDs)
+
+
+		# evalIsImpossibles = evalIsImpossibles.view(-1, 1)
+		# evalIsImpossiblesNeg = 1 - evalIsImpossibles
+		# evalIsImpossibles = torch.cat((evalIsImpossiblesNeg, evalIsImpossibles), dim=1).float()
+
+		# precision = precision_score(evalIsImpossibles.detach().cpu(), batchIsImpossible.detach().cpu() > 0.5, average="micro")
+		# recall = recall_score(evalIsImpossibles.detach().cpu(), batchIsImpossible.detach().cpu() > 0.5, average="micro")
+		# f1 = f1_score(evalIsImpossibles.detach().cpu(), batchIsImpossible.detach().cpu() > 0.5, average="micro")
+
+		# print("Epoch: {} - Precision: {}, Recall: {}, F1: {}".format(epoch, precision, recall, f1))
+
+	modelToSave = model.module if hasattr(model, "module") else model
+
+	print("Saving model...")
+	outputModelFile = outputDir + "/isImpossibleTrained.bin"
+	torch.save(modelToSave.state_dict(), outputModelFile)
+
+	print("Predicting...")
+
+	model.to(device)
+	model.eval()
+
+	for inputIDs, inputMask, segmentIDs, exampleIndices, isImpossibles in tqdm(evalDataLoader, desc="Evaluating"):
+
+		inputIDs = inputIDs.to(device)
+		inputMask = inputMask.to(device)
+		segmentIDs = segmentIDs.to(device)
+		exampleIndices = exampleIndices.to(device)
+		isImpossibles = isImpossibles.to(device)
 
 		with torch.no_grad():
-			batchStartLogits, batchEndLogits, batchIsImpossible = model(evalBatchInputIDs, evalBatchInputMask, evalBatchSegmentIDs)
+			_, _, isImpossibleComputed = model(inputIDs, inputMask, segmentIDs)
 
+		isImpossibles = isImpossibles.view(-1, 1)
+		isImpossiblesNeg = 1 - isImpossibles
+		isImpossibles = torch.cat((isImpossiblesNeg, isImpossibles), dim=1).float()
 
-		evalIsImpossibles = evalIsImpossibles.view(-1, 1)
-		evalIsImpossiblesNeg = 1 - evalIsImpossibles
-		evalIsImpossibles = torch.cat((evalIsImpossiblesNeg, evalIsImpossibles), dim=1).float()
+		classWeights = torch.tensor([1., 2.])
+		weightedLossFun = BCELoss(weight=classWeights).cuda()
+		loss = weightedLossFun(isImpossibleComputed, isImpossibles)
 
-		precision = precision_score(evalIsImpossibles.detach().cpu(), batchIsImpossible.detach().cpu() > 0.5, average="micro")
-		recall = recall_score(evalIsImpossibles.detach().cpu(), batchIsImpossible.detach().cpu() > 0.5, average="micro")
-		f1 = f1_score(evalIsImpossibles.detach().cpu(), batchIsImpossible.detach().cpu() > 0.5, average="micro")
+		if nGPU > 1:
+			loss = loss.mean()
 
-		print("Epoch: {} - Precision: {}, Recall: {}, F1: {}".format(epoch, precision, recall, f1))
+		#loss.backward()
 
+		#optimizer.step()
+		optimizer.zero_grad()
+
+		precision = precision_score(isImpossibles.detach().cpu(), isImpossibleComputed.detach().cpu() > 0.5, average="micro")
+		recall = recall_score(isImpossibles.detach().cpu(), isImpossibleComputed.detach().cpu() > 0.5, average="micro")
+		f1 = f1_score(isImpossibles.detach().cpu(), isImpossibleComputed.detach().cpu() > 0.5, average="micro")
+
+		tqdm.write("Loss: {}, Precision: {}, Recall: {}, F1: {}".format(loss, precision, recall, f1))
+
+	"""
 	for l in deactivatedLayers:
 		for v in l.parameters():
 			v.requires_grad = True
@@ -250,7 +307,7 @@ def main():
 		outputNullLogOddsFile = os.path.join(outputDir, "null_odds.json")
 
 		writePredictions(evalExamples, evalFeatures, allResults, 20, 30, True, outputPredFile, outputNBestFile, outputNullLogOddsFile, usingV2=True, nullDiffThreshold=0.0)
-
+	"""
 	
 	
 if __name__ == "__main__":

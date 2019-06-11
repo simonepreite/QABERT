Fix isImpossible data for loss calculation

BERT.py
@@ -82,7 +82,7 @@ class QABERT(BERTInitializer):
 		self.bert = BERTModel(hiddenSize, numLayers, numAttentionHeads, vocabSize, dropout)
 		self.apply(self.weightsInitialization)
 		self.midIsImpossibleLinear = nn.Linear(hiddenSize, 256)
-		self.isImpossibleOutput = nn.Linear(512*256, 1) #TODO: 512 sequence length, da parametrizzare
+		self.isImpossibleOutput = nn.Linear(512*256, 2) #TODO: 512 sequence length, da parametrizzare
 		self.qaLinear1 = nn.Linear(hiddenSize + 256, hiddenSize + 256)
 		self.qaLinear2 = nn.Linear(hiddenSize + 256, 64)
 		self.qaOutput = nn.Linear(64, 2)
@@ -101,6 +101,7 @@ class QABERT(BERTInitializer):
 #		print("midIsImpOutputFlattened shape:", midIsImpOutputFlattened.size())
 
 		isImpOutput = self.isImpossibleOutput(midIsImpOutputFlattened)
+		isImpOutput = isImpOutput.squeeze()
 #		print("self.isImpossibleOutput later: {} - output shape: {}".format(self.isImpossibleOutput, isImpOutput.size()))
 
 		

main.py
@@ -129,9 +129,14 @@ def main():
 			inputIDs, inputMask, segmentIDs, startPositions, endPositions, isImpossibles = batch
 			print("inputIDs: {}, segmentIDs: {}, isImpossibles: {}".format(inputIDs.size(), segmentIDs.size(), isImpossibles.size()))
 			_, _, isImpossibleComputed = model(inputIDs, inputMask, segmentIDs)
-			print("isImpossibleComputed: {}".format(isImpossibleComputed.size()))
+			print("isImpossibleComputed: {}\n{}".format(isImpossibleComputed.size(), isImpossibleComputed))
 
-			classWeights = torch.tensor([1, 2])
+			isImpossibles = isImpossibles.view(-1, 1)
+			isImpossiblesNeg = 1 - isImpossibles
+			isImpossibles = torch.cat((isImpossiblesNeg, isImpossibles), dim=1)
+			print("isImpossibles GT:\n", isImpossibles)
+
+			classWeights = torch.tensor([1., 2.])
 			weightedLossFun = BCELoss(weight=classWeights)
 			loss = weightedLossFun(isImpossibleComputed, isImpossibles)
 
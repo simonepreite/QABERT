64c47a3cc160e7c17cfbbee360811a75cf8f035e
Matteo Del Vecchio <delvecchiomatteo96@gmail.com>
2019-10-29 01:36:50 +0100

    update to multiprocess featurization

--- SQuADDataset.py
+++ SQuADDataset.py
@@ -138,6 +138,133 @@ def computeSoftmax(scores):
 	return probs
 	"""
 	
+def parallelFeaturization(examples, tokenizer, maxSeqLength, docStride, maxQueryLength, trainingMode, chunkSize, startIndex):
+	#assert len(filenames) == (len(examples) // chunkSize + 1) if len(examples) % chunkSize else len(examples) // chunkSize
+	features = []
+
+	print("parallel featurization with startIndex: {}".format(startIndex))
+
+	indexes = []
+	for (index, example) in enumerate(examples, start=startIndex):
+		indexes.append(index)
+		queryTokens = tokenizer.tokenize(example.questionText)
+
+		if len(queryTokens) > maxQueryLength:
+			queryTokens = queryTokens[0:maxQueryLength]
+
+		tokenFirstWordpieceIndex = []
+		firstWordpieceTokenIndex = []
+		wordpieceParagraph = []
+
+		for (i, token) in enumerate(example.parWords):
+			firstWordpieceTokenIndex.append(len(wordpieceParagraph))
+			subTokens = tokenizer.tokenize(token)
+			for subToken in subTokens:
+				tokenFirstWordpieceIndex.append(i)
+				wordpieceParagraph.append(subToken)
+
+		tokStartPos = None
+		tokEndPos = None
+		if trainingMode:
+			if example.isImpossible:
+				tokStartPos = -1
+				tokEndPos = -1
+			else:
+				tokStartPos = firstWordpieceTokenIndex[example.startPos]
+				if example.endPos < len(example.parWords) - 1:
+					tokEndPos = firstWordpieceTokenIndex[example.endPos + 1] -1
+				else:
+					tokEndPos = len(wordpieceParagraph) -1
+				(tokStartPos, tokEndPos) = improveAnswerExtent(wordpieceParagraph, tokStartPos, tokEndPos, tokenizer, example.answerText)
+
+		maxTokensForChunks = maxSeqLength - len(queryTokens) - 3
+
+		parChunks = []
+		startOffset = 0
+		while startOffset < len(wordpieceParagraph):
+			length = len(wordpieceParagraph) - startOffset
+			if length > maxTokensForChunks:
+				length = maxTokensForChunks
+			parChunks.append(DocSpan(start=startOffset, length=length))
+			if startOffset + length == len(wordpieceParagraph):
+				break
+			startOffset += min(length, docStride)
+
+		for (parChunkIndex, parChunk) in enumerate(parChunks):
+			tokens = []
+			tokenFirstWordpieceMap = {}
+			tokenMostRelevantChunk = {}
+			segmentIDs = []
+
+			tokens.append("[CLS]")
+			segmentIDs.append(0)
+
+			for token in queryTokens:
+				tokens.append(token)
+				segmentIDs.append(0)
+
+			tokens.append("[SEP]")
+			segmentIDs.append(0)
+
+			for i in range(parChunk.length):
+				splitTokenIndex = parChunk.start + i
+				tokenFirstWordpieceMap[len(tokens)] = tokenFirstWordpieceIndex[splitTokenIndex]
+				isMostRelevant = isMostRelevantParagraphChunk(parChunks, parChunkIndex, splitTokenIndex)
+				tokenMostRelevantChunk[len(tokens)] = isMostRelevant
+				tokens.append(wordpieceParagraph[splitTokenIndex])
+				segmentIDs.append(1)
+
+			tokens.append("[SEP]")
+			segmentIDs.append(1)
+
+			bertInputIDs = tokenizer.tokensToIDs(tokens)
+			bertInputMask = [1] * len(bertInputIDs)
+
+			while len(bertInputIDs) < maxSeqLength:
+				bertInputIDs.append(0)
+				bertInputMask.append(0)
+				segmentIDs.append(0)
+
+			assert len(bertInputIDs) == maxSeqLength
+			assert len(bertInputMask) == maxSeqLength
+			assert len(segmentIDs) == maxSeqLength
+
+			startPosition = None
+			endPosition = None
+
+			if trainingMode:
+				if example.isImpossible:
+					startPosition = 0
+					endPosition = 0
+				else:
+					chunkStart = parChunk.start
+					chunkEnd = parChunk.start + parChunk.length - 1
+					if not (tokStartPos >= chunkStart and tokEndPos <= chunkEnd):
+						startPosition = 0
+						endPosition = 0
+					else:
+						chunkOffset = len(queryTokens) + 2 # because of tags like [CLS] or [SEP]
+						startPosition = tokStartPos - chunkStart + chunkOffset
+						endPosition = tokEndPos - chunkStart + chunkOffset
+
+			uniqueID = random.randint(100000000, 999999999)
+			inputFeat = InputFeatures(ID=uniqueID,
+									  exampleID=index,
+									  chunkID=parChunkIndex,
+									  tokens=tokens,
+									  tokenFirstWordpieceMap=tokenFirstWordpieceMap,
+									  tokenMostRelevantChunk=tokenMostRelevantChunk,
+									  inputIDs=bertInputIDs,
+									  inputMask=bertInputMask,
+									  segmentIDs=segmentIDs,
+									  startPos=startPosition,
+									  endPos=endPosition,
+									  isImpossible=example.isImpossible)
+#			fileFeatures.append(inputFeat)
+			features.append(inputFeat)
+			#uniqueID += 1
+	#print("indexes: {}".format(indexes))
+	return features
 
 
 def featurizeExamples(examples, tokenizer, maxSeqLength, docStride, maxQueryLength, trainingMode):
